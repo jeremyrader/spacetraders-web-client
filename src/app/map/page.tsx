d0554930d53495b3c1d7d9d094697ba2f7889d3a
Jeremy Rader <jeremy@jeremyrader.com>
2024-08-12 19:30:33 -0600

    refactor: remove contracts from map page

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -20,14 +20,7 @@ function Dashboard() {
       setAgent(result.data)
     }
 
-    async function fetchContracts() {
-      const result = await fetchResource('my/contracts')
-      // only handles the first page for now
-      setContracts(result.data);
-    }
-
     fetchAgent();
-    fetchContracts();
   }, []);
 
   const handleSelectMap = (map: string, system: System) => {
@@ -52,41 +45,6 @@ function Dashboard() {
         <p>Credits: {agent?.credits}</p>
         <p>Starting Faction: {agent?.startingFaction}</p>
         <p>Ship Count: {agent?.shipCount}</p>
-
-        <p className="mt-8">Contracts:</p>
-
-        {
-          contracts.map((contract, index) => (
-            <div className="mt-4" key={index}>
-              <p>Type: {contract.type}</p>
-              <p>
-                Terms:
-              </p>
-              <p>Deadline: {contract.terms.deadline}</p>
-              <p>Payment on Accept: {contract.terms.payment.onAccepted}</p>
-              <p>Payment on Fulfillment: {contract.terms.payment.onFulfilled}</p>
-
-              <p>Deliveries:</p>
-
-              {
-                contract.terms.deliver.map((delivery, index) => (
-                  <div className="my-4" key={index}>
-                    <p>Trade Symbol: {delivery.tradeSymbol}</p>
-                    <p>Trade Destination: {delivery.destinationSymbol}</p>
-                    <p>Units Required: {delivery.unitsRequired}</p>
-                    <p>Units Fulfilled: {delivery.unitsFulfilled}</p>
-                  </div>
-                ))
-              }
-
-              <p>Accepted: {contract.accepted ? 'Yes': 'No'}</p>
-              <p>Fulfilled: {contract.fulfilled ? 'Yes': 'No'}</p>
-              <p>Expiration: {contract.expiration}</p>
-              <p>Deadline to Accept: {contract.deadlineToAccept}</p>
-
-            </div>
-          ))
-        }
       </div>
       
 
